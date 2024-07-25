0b0b9f07a589842f3a9a7d944eb5ec6fbc3eac06
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-25 15:52:18 +0700

    created a function for fetching new data anime

---
+++ src/lib/utils.ts
@@ -0,0 +1,17 @@
+import type { AnimeProp } from '$lib';
+
+export const fetchNewData = async (pageNumber: Number) => {
+	let request = await fetch(
+		`https://shikimori.one/api/animes?page=${pageNumber}&limit=8&order=popularity`
+	);
+	let animes;
+
+	try {
+		let response = await request.json();
+		animes = response;
+	} catch (error) {
+		console.log(error);
+	}
+
+	return { animes } as { animes: AnimeProp[] };
+};
