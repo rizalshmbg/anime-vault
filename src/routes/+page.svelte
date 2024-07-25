3a2e88aeb20eef67ef65ba11f711ce73cf8e5364
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-25 14:32:08 +0700

    configuration of structure of web

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import AnimeCard from '$lib/components/AnimeCard.svelte';
+	import Spinner from '$lib/components/Spinner.svelte';
+
+	let { data } = $props();
+</script>
+
+<div class="max-w-7xl mx-auto bg-[#0F1117]">
+	<main class="sm:p-16 py-16 px-8 flex flex-col gap-10">
+		<h2 class="text-3xl text-white font-bold">Explore Anime</h2>
+
+		<section class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+			{#each data.animes as anime}
+				<AnimeCard {...anime} />
+			{/each}
+		</section>
+
+		<div id="spinner-container">
+			<Spinner />
+		</div>
+	</main>
+</div>
